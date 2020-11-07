12f504c0fbc9d1fa50d04ada4b0f6c2987ffdafe
Johnson Chandra <johnson.chandra@gmail.com>
2020-11-07 15:43:27 +0700

    refactor ListImagePrice to ImagePrice

---
+++ apps/common/ui/components/ImagePrice/index.js
@@ -0,0 +1,28 @@
+import { Meteor } from 'meteor/meteor';
+
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+
+const ImagePrice = (props) => {
+  const { doc } = props;
+  return (
+    <Link key={doc._id} to={doc.linkUrl}>
+      <div className="hover-style">
+        <img src={doc.logoUrl || Meteor.settings.public.default.logoUrl} alt={doc.name} />
+        <div className="shop-title">
+          <span>{doc.name}</span>
+          <br />
+          <span className="new-price">{doc.price}</span>
+          <span className="old-price">{doc.priceOld}</span>
+        </div>
+      </div>
+    </Link>
+  );
+};
+
+ImagePrice.propTypes = {
+  doc: PropTypes.object.isRequired,
+};
+
+export default ImagePrice;
