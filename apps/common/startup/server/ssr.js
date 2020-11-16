cf1438bbee3edc97ee4da1376f9d101d0dc7673c
Johnson Chandra <johnson.chandra@gmail.com>
2020-11-16 13:22:26 +0700

    fix ssr, added accidentally removed bootstrap

--- apps/common/startup/server/ssr.js
+++ apps/common/startup/server/ssr.js
@@ -41,6 +41,8 @@ onPageLoad(async (sink) => {
   sink.appendToHead(
     `<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.10/css/all.css" integrity="sha384-+d0P83n9kaQMCwj8F4RJB66tzIwOKmrdb46+porD/OvrJ+37WqIM7UoBtwHO6Nlg" crossorigin="anonymous">`,
   );
+  sink.appendToHead(`<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+  `);
   sink.appendToHead(`<link rel="shortcut icon" href="${tenant.settings.logo || '/mkcb.ico'}">`);
   sink.appendToHead(
     `<link rel="apple-touch-icon" sizes="120x120" href="${tenant.settings.logo ||
