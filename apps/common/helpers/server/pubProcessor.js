1bb7e7c63aa73e87a96c4aa9747bb6b332453963
Johnson Chandra <johnson.chandra@gmail.com>
2021-01-12 14:35:40 +0700

    add additionalCounts to pubProcessor.js

--- apps/common/helpers/server/pubProcessor.js
+++ apps/common/helpers/server/pubProcessor.js
@@ -2,10 +2,23 @@ import { Counts } from 'meteor/tmeasday:publish-counts';
 
 import getQueryAndProjection from './getQueryAndProjection';
 
-const pubProcessor = (Entity, publishName, getJSONdefs, props, parent) => {
+const pubProcessor = (Entity, publishName, getJSONdefs, props, parent, additionalCounts) => {
   const { query, projection } = getQueryAndProjection(publishName, getJSONdefs, props, parent);
 
   Counts.publish(parent, `${publishName}Count`, Entity.find(query));
+
+  if (additionalCounts)
+    additionalCounts.forEach((additionalCount) => {
+      const queryAdditional = {
+        ...query,
+        ...additionalCount.query,
+      };
+      Counts.publish(
+        parent,
+        `${publishName}_${additionalCount.name}_Count`,
+        Entity.find(queryAdditional),
+      );
+    });
   return Entity.find(query, projection);
 };
 
